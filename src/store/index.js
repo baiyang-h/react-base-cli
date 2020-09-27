63bfc73b9ab2e790297034a9f1c619461dad0e49
hhq <7900124+earthly_guest@user.noreply.gitee.com>
2020-09-28 00:17:19 +0800

    mod: 状态管理

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@
  * 状态管理
  */
 import { combineReducers, createStore, applyMiddleware, compose } from 'redux'
-import createSagaMiddleware from 'redux-saga'
+// import createSagaMiddleware from 'redux-saga'
 import { all } from 'redux-saga/effects'
 import thunkMiddleware from 'redux-thunk';
 import promiseMiddleware from 'redux-promise';
@@ -10,14 +10,10 @@ import promiseMiddleware from 'redux-promise';
 import userSaga  from './modules/user/saga'
 
 import { namespace as appNamespace, appReducer } from './modules/app/reducer'
-import { namespace as permissionNamespace, permissionReducer} from './modules/permission/reducer'
-import { namespace as tagsViewNamespace, tagsViewReducer} from './modules/tagsView/reducer'
 import { namespace as userNamespace, userReducer } from './modules/user/reducer'
 
 const reducers = combineReducers({
   [appNamespace]: appReducer,
-  [permissionNamespace]: permissionReducer,
-  [tagsViewNamespace]: tagsViewReducer,
   [userNamespace]: userReducer
 })
 
@@ -31,23 +27,27 @@ compose(fn1, fn2):
   }
 */
 
-export const sagaMiddleware = createSagaMiddleware();
+// export const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   reducers,
   compose(
-    applyMiddleware(thunkMiddleware, promiseMiddleware, sagaMiddleware),
+    applyMiddleware(
+      thunkMiddleware,
+      promiseMiddleware,
+      /*sagaMiddleware*/
+    ),
     reduxDebug
   )
 )
 
-export function* rootSaga() {
-  yield all([
-    ...userSaga,
-  ])
-}
+// export function* rootSaga() {
+//   yield all([
+//     ...userSaga,
+//   ])
+// }
 
-sagaMiddleware.run(rootSaga);
+// sagaMiddleware.run(rootSaga);
 
 export default store
 
--- src/store/modules/permission/action.js
+++
@@ -1,7 +0,0 @@
-import { createActions } from 'redux-actions';
-
-export const { aaa } = createActions({
-  AAA: roles => new Promise(resolve => {
-    resolve()
-  })
-})
