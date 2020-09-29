4f1e61c72be43f0bf15123274d6e1ab1961675fd
hhq <7900124+earthly_guest@user.noreply.gitee.com>
2020-09-29 08:48:57 +0800

    mod: 增加样式、路由

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
+// 组件
+import Home from '@/pages/Home'
+import Layout from '@/pages/Layout'
+import Login from '@/pages/Login'
+import Page404 from '@/pages/Page404'
 
 function App() {
   return (
     <div className="App">
-      App
+      <Router>
+        <div>
+          <Link to='/'>Home</Link>
+          <Link to='/app'>Layout</Link>
+          <Link to='/login'>Login</Link>
+          <Link to='/Page404'>Page404</Link>
+        </div>
+        <Switch>
+          <Route exact path="/" component={ Home } />
+          <Route path="/app" component={ Layout } />
+          <Route exact path="/login" component={ Login } />
+          <Route component={ Page404 } />
+        </Switch>
+      </Router>
     </div>
   );
 }
