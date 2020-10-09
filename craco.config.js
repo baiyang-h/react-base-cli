5d6ec5f0d34938185e297dfda120b05528ac0113
hhq <honghq@weipaitang.com>
2020-10-09 17:43:33 +0800

    mod: 增加 craco.config.js 文件， 这是不暴露 webpack 配置文件的情况

--- craco.config.js
+++ craco.config.js
@@ -1,12 +1,3 @@
-具体细节查看 master 分支，该分支对各个配置进行了展开，这里只针对 `@craco/craco` 模块，当我们在不暴露 `webapck` 配置文件的情况下，即不执行 `npm run eject`，通过该模块来进行设置。
-
-```bash
-yarn add @craco/craco
-```
-
-具体配置可以查看[文档](!https://www.npmjs.com/package/@craco/craco#configuration-overview)，以下只显示了部分内容
-
-```js
 const path = require('path')
 const mockServer = require('./mock/mock-server')
 const sassResourcesLoader = require('craco-sass-resources-loader');
@@ -54,7 +45,3 @@ module.exports = {
         }
     },
 }
-```
-
-使用 `sassResourcesLoader` 依赖，来配置 sass 全局变量。
-
